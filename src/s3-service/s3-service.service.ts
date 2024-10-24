878d5d34f01baa3e45a42a7662b4850c2de4c075
MunSeongyun <borygashill608@gmail.com>
2024-10-24 17:01:49 +0900

    💡 feat: Create endpoint for downloading MD files from S3
    - Implemented endpoint to download MD files from S3 bucket.
    - Changed upload mechanism to place files within specific folders instead of directly in the bucket.

--- src/s3-service/s3-service.service.ts
+++ src/s3-service/s3-service.service.ts
@@ -5,6 +5,7 @@ import { v4 as uuid } from 'uuid';
 @Injectable()
 export class S3ServiceService {
   private s3: AWS.S3;
+  private bucketName:string;
 
   constructor(private configService: ConfigService) {
     this.s3 = new AWS.S3({
@@ -12,20 +13,28 @@ export class S3ServiceService {
       secretAccessKey: this.configService.get<string>('AWS_SECRET_ACCESS_KEY'),
       region: this.configService.get<string>('AWS_REGION'),
     });
+    this.bucketName = this.configService.get<string>('AWS_S3_BUCKET_NAME');
   }
 
   async uploadMdFile(content: string, fileName?: string): Promise<AWS.S3.ManagedUpload.SendData> {
-    const bucketName = this.configService.get<string>('AWS_S3_BUCKET_NAME');
     
-    const key = fileName ? `${fileName}-${uuid()}.md` : `${uuid()}.md`;
+    const key = fileName ? `incorrect-notes/${fileName}-${uuid()}.md` : `incorrect-notes/${uuid()}.md`;
 
     const params: AWS.S3.PutObjectRequest = {
-      Bucket: bucketName,
-      Key: key,         // S3에서 저장될 파일 이름
+      Bucket: this.bucketName,
+      Key: key,         // S3에서 저장될 파일 이름 (incorrect-notes 폴더 내에 저장)
       Body: content,     // 파일 내용 (문자열)
       ContentType: 'text/plain',  // 파일 MIME 타입 설정
     };
 
     return await this.s3.upload(params).promise();
+  }
+
+  async downloadMdFile(fileName: string) {
+    const params: AWS.S3.GetObjectRequest = {
+      Bucket: this.bucketName,
+      Key: `incorrect-notes/${fileName}`,  // incorrect-notes 폴더 내의 파일을 가져옴
+    };
+    return await this.s3.getObject(params).promise();
   }
 }
