010ce620c467b0e325bf8cbdd06451baaa382b24
MunSeongyun <borygashill608@gmail.com>
2024-10-29 19:37:29 +0900

    🔨refactor: Update httpsOptions to return values based on environment variables
    - Modified httpsOptions to return different configurations depending on environment variables.

--- src/httpsOptions.ts
+++ src/httpsOptions.ts
@@ -1,6 +1,11 @@
 import * as fs from 'fs';
 
-export const httpsOptions = {
-  key:  fs.readFileSync('./key.pem'),
-  cert:  fs.readFileSync('./cert.pem')
-}
+export const httpsOptions = process.env.NODE_ENV === 'EC2' 
+  ? {
+      key: process.env.SSL_KEY,
+      cert: process.env.SSL_CERT
+    }
+  : {
+      key: fs.readFileSync('./key.pem'),
+      cert: fs.readFileSync('./cert.pem')
+    };
