0d9212ebc53a507de6ab2b6d3b55d1964c053508
MunSeongyun <borygashill608@gmail.com>
2024-11-04 17:37:53 +0900

    📚 Docs : swagger update

--- src/incorrect-note/dto/save-incorrect-note.dto.ts
+++ src/incorrect-note/dto/save-incorrect-note.dto.ts
@@ -2,12 +2,20 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsString, Matches, IsIn } from 'class-validator';
 
 export class SaveIncorrectNoteDto {
+  @ApiProperty({
+    example: 'Python',
+    description: '언어'
+  })
   @IsString()
   @Matches(/^[A-Za-z]+$/, {
     message: '언어는 영문 알파벳만 사용할 수 있습니다.'
   })
   language: string;
 
+  @ApiProperty({
+    example: '1',
+    description: '오답 유형'
+  })
   @IsIn(['1', '2', '3', '4'], {
     message: 'errorType은 1, 2, 3, 4 중 하나여야 합니다.'
   })
